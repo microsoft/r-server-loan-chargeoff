df2b1ef9647ec334024c88eb6863b8a72866944d
v-robwhi <v-robwhi@microsoft.com>
2018-02-02 12:27:33 -0800

    Dev (#5)
    * First Commit
    
    * commit test
    
    * git is case sensitive , that is really dumb
    
    * testing data population
    
    * added more csv files
    
    * Loan_Data1000 added
    
    * more files
    
    * deleting data files
    
    * added 10k csv files
    
    * added 100k csv files
    
    * 1million csv
    
    * more 1 million csv files
    
    * even more 1 million records csv files
    
    * added unzip archive
    
    * data file unzip
    
    * added files to pull
    
    * playing with csv files
    
    * comment out unzipping routine
    
    * file stuff
    
    * removed old db name
    
    * fixed path
    
    * update file places
    
    * checking in
    
    * trying bcp
    
    * turned off sample web as it is not needed
    
    * updated to use txt files
    
    * added force to bcp
    
    * removed the force
    
    * come to the dark side , we have cookies
    
    * got rid of header in txt file
    
    * got rid of pipe delimeter
    
    * updated txt files
    
    * removed header row from txt files
    
    * futzing with bcp
    
    * trying bulk insert
    
    * figuring out bulk insert
    
    * hardcoded bulk inserts
    
    * added proper tick marks
    
    * added comments
    
    * added support for 100K records
    
    * updated sql objects for 100k records
    
    * Fixed a ton of stuff
    
    * got rid of jupyter notebook password
    
    * took out 100k imports
    
    * fixed sproc name
    
    * Updated Setup Path
    
    * added standalone
    
    * set standalone to no for testing
    
    * if then else for Standalone
    
    * yep
    
    * yes
    
    * nk
    
    * trying to get isStandalone working
    
    * trying to get standalone working
    
    * final push

---
+++ R/chargeoff_batch_prediction.R
@@ -0,0 +1,37 @@
+library(RevoScaleR)
+library(MicrosoftML)
+###########################################################################################################################################
+# Function for demonstrating MicrosoftML's selectFeatures and categorical transforms.
+# 
+# Parameters:
+#                * connection_string - substitute appropriate username and password along with database name and server if needed
+#                * best_models_file - file where best_model object from training/testing step is stored
+#                * score_set - table name of for scoring data (usually the prefix of 10k/100k/1m will only change based on data set size)
+#                * score_prediction - table name where to store prediction results
+#
+# Pre-Requisites:
+#                1. Make sure Loan_ChargeOff.ps1 has been run for your appropriate size data set so the required tables have already been 
+#                   created and dataset imported (it's already been run for 10k loans data set)
+#                2. Modelling must have been completed
+#                   
+###########################################################################################################################################
+batch_score <- function (connection_string = "Driver=SQL Server;Server=.;Database=LoanChargeOff_R;Trusted_Connection=True",
+                         best_models_file = "loan_chargeoff_best_model_10k.rdata",
+                         score_set = "loan_chargeoff_score_10k",
+                         score_prediction = "loan_chargeoff_prediction_10k_R")
+{
+    load(best_models_file)
+    if (!exists("best_model"))
+    {
+      stop("best_models_file does not contain best_model object, make sure you saved it properly during training step.")
+    }
+    cc <- RxInSqlServer(connectionString = connection_string)
+    rxSetComputeContext(cc)
+    scoring_data <- RxSqlServerData(table = score_set, connectionString = connection_string)
+    prediction_data <- RxSqlServerData(table = score_prediction, connectionString = connection_string)
+    
+    # Warning: this will drop and recreate the prediction table
+    rxPredict(best_model$model, scoring_data, outData = prediction_data, extraVarsToWrite = c("loanId", "payment_date"), overwrite=TRUE)
+}
+
+batch_score()
---
+++ Resources/ActionScripts/SetupVM.ps1
@@ -0,0 +1,288 @@
+
+
+[CmdletBinding()]
+param(
+[parameter(Mandatory=$True, Position=1)]
+[ValidateNotNullOrEmpty()] 
+[string]$serverName,
+
+[parameter(Mandatory=$True, Position=2)]
+[ValidateNotNullOrEmpty()] 
+[string]$username,
+
+[parameter(Mandatory=$True, Position=3)]
+[ValidateNotNullOrEmpty()] 
+[string]$password,
+
+[parameter(Mandatory=$false, Position=4)]
+[ValidateNotNullOrEmpty()] 
+[string]$baseurl,
+
+
+[parameter(Mandatory=$false, Position=5)]
+[ValidateNotNullOrEmpty()] 
+[string]$Prompt
+)
+$startTime = Get-Date
+
+
+
+#$Prompt= if ($Prompt -match '^y(es)?$') {'Y'} else {'N'}
+$Prompt = 'N'
+
+if($baseurl -eq $null) {$isStandalone = 'Y'} ELSE {$isStandalone = 'N'}
+
+#$isStandAlone = 'N'
+
+
+##Change Values here for Different Solutions 
+$SolutionName = "LoanChargeOff"
+$SolutionFullName = "r-server-loan-chargeoff" 
+$JupyterNotebook = "LoanChargeOff.ipynb"
+$Shortcut = "LoanChargeOffHelp.url"
+
+
+### DON'T FORGET TO CHANGE TO MASTER LATER...
+$Branch = "Dev" 
+$InstallR = 'Yes'  ## If Solution has a R Version this should be 'Yes' Else 'No'
+$InstallPy = 'No' ## If Solution has a Py Version this should be 'Yes' Else 'No'
+$SampleWeb = 'No' ## If Solution has a Sample Website  this should be 'Yes' Else 'No' 
+$EnableFileStream = 'No' ## If Solution Requires FileStream DB this should be 'Yes' Else 'No' 
+$Prompt = 'N'
+
+
+$setupLog = "c:\tmp\setup_log.txt"
+Start-Transcript -Path $setupLog -Append
+$startTime = Get-Date
+Write-Host -ForegroundColor 'Green'  "  Start time:" $startTime 
+
+
+###These probably don't need to change , but make sure files are placed in the correct directory structure 
+$solutionTemplateName = "Solutions"
+$solutionTemplatePath = "C:\" + $solutionTemplateName
+$checkoutDir = $SolutionName
+$SolutionPath = $solutionTemplatePath + '\' + $checkoutDir
+$desktop = "C:\Users\Public\Desktop\"
+$scriptPath = $SolutionPath + "\Resources\ActionScripts\"
+$SolutionData = $SolutionPath + "\Data\"
+
+
+
+####$Query = "SELECT SERVERPROPERTY('ServerName')"
+##$si = invoke-sqlcmd -Query $Query
+##$si = $si.Item(0)
+
+
+###$serverName = if($serverName -eq $null) {$si}
+
+##WRITE-HOST " ServerName set to $ServerName"
+
+
+
+##########################################################################
+#Clone Data from GIT
+##########################################################################
+
+
+$clone = "git clone --branch $Branch --single-branch https://github.com/Microsoft/$SolutionFullName $solutionPath"
+
+if (Test-Path $SolutionPath) { Write-Host " Solution has already been cloned"}
+ELSE {Invoke-Expression $clone}
+
+If ($InstalR -eq 'Yes')
+{
+Write-Host -ForeGroundColor magenta "Installing R Packages"
+Set-Location "C:\Solutions\$SolutionName\Resources\ActionScripts\"
+# install R Packages
+Rscript install.R 
+}
+
+
+if ($isStandAlone -eq 'N')
+{
+cd $SolutionData
+
+# List of data files to be downloaded
+$dataList = "loan_info_10k", "member_info_10k", "payments_info_10k", "loan_info_100k", "member_info_100k", "payments_info_100k", "loan_info_1m", "member_info_1m", "payments_info_1m"
+$dataExtn = ".csv"
+$hashExtn = ".hash"
+foreach ($dataFile in $dataList)
+{
+    $down = $baseurl + '/' + $dataFile + $dataExtn
+    Write-Host -ForeGroundColor 'magenta' "Downloading file $down..."
+    Start-BitsTransfer -Source $down  
+}
+}
+#################################################################
+##DSVM Does not have SQLServer Powershell Module Install or Update 
+#################################################################
+
+
+
+Write-Host " Installing SQLServer Power Shell Module or Updating to latest "
+
+if (Get-Module -ListAvailable -Name SQLServer) {Update-Module -Name "SQLServer"}
+ else 
+    {
+    Install-Module -Name SQLServer -Scope AllUsers -AllowClobber -Force
+    Import-Module -Name SQLServer
+    }
+
+
+## if FileStreamDB is Required Alter Firewall ports for 139 and 445
+if ($EnableFileStream -eq 'Yes')
+    {
+    netsh advfirewall firewall add rule name="Open Port 139" dir=in action=allow protocol=TCP localport=139
+    netsh advfirewall firewall add rule name="Open Port 445" dir=in action=allow protocol=TCP localport=445
+    Write-Host -ForeGroundColor cyan " Firewall as been opened for filestream access..."
+    }
+If ($EnableFileStream -eq 'Yes')
+    {
+    Set-Location "C:\Program Files\Microsoft\ML Server\PYTHON_SERVER\python.exe" 
+    .\setup.py install
+    Write-Host -ForeGroundColor cyan " Py Instal has been updated to latest version..."
+    }
+
+
+############################################################################################
+#Configure SQL to Run our Solutions 
+############################################################################################
+
+#Write-Host -ForegroundColor 'Cyan' " Switching SQL Server to Mixed Mode"
+
+
+### Change Authentication From Windows Auth to Mixed Mode 
+Invoke-Sqlcmd -Query "EXEC xp_instance_regwrite N'HKEY_LOCAL_MACHINE', N'Software\Microsoft\MSSQLServer\MSSQLServer', N'LoginMode', REG_DWORD, 2;" -ServerInstance "LocalHost" 
+
+Write-Host -ForeGroundColor 'cyan' " Configuring SQL to allow running of External Scripts "
+### Allow Running of External Scripts , this is to allow R Services to Connect to SQL
+Invoke-Sqlcmd -Query "EXEC sp_configure  'external scripts enabled', 1"
+
+### Force Change in SQL Policy on External Scripts 
+Invoke-Sqlcmd -Query "RECONFIGURE WITH OVERRIDE" 
+Write-Host -ForeGroundColor 'cyan' " SQL Server Configured to allow running of External Scripts "
+
+### Enable FileStreamDB if Required by Solution 
+if ($EnableFileStream -eq 'Yes') 
+    {
+# Enable FILESTREAM
+        $instance = "MSSQLSERVER"
+        $wmi = Get-WmiObject -Namespace "ROOT\Microsoft\SqlServer\ComputerManagement14" -Class FilestreamSettings | where-object {$_.InstanceName -eq $instance}
+        $wmi.EnableFilestream(3, $instance)
+        Stop-Service "MSSQ*" -Force
+        Start-Service "MSSQ*"
+ 
+        Set-ExecutionPolicy Unrestricted
+        #Import-Module "sqlps" -DisableNameChecking
+        Invoke-Sqlcmd "EXEC sp_configure filestream_access_level, 2"
+        Invoke-Sqlcmd "RECONFIGURE WITH OVERRIDE"
+        Stop-Service "MSSQ*"
+        Start-Service "MSSQ*"
+    }
+ELSE
+    { 
+    Write-Host -ForeGroundColor 'cyan' " Restarting SQL Services "
+    ### Changes Above Require Services to be cycled to take effect 
+    ### Stop the SQL Service and Launchpad wild cards are used to account for named instances  
+    Restart-Service -Name "MSSQ*" -Force
+}
+### Start the SQL Service 
+#Start-Service -Name "MSSQ*"
+#Write-Host -ForegroundColor 'Cyan' " SQL Services Restarted"
+
+
+$Query = "CREATE LOGIN $username WITH PASSWORD=N'$password', DEFAULT_DATABASE=[master], CHECK_EXPIRATION=OFF, CHECK_POLICY=OFF"
+Invoke-Sqlcmd -Query $Query -ErrorAction SilentlyContinue
+
+$Query = "ALTER SERVER ROLE [sysadmin] ADD MEMBER $username"
+Invoke-Sqlcmd -Query $Query -ErrorAction SilentlyContinue
+
+
+
+##Unzip Data Files 
+#Expand-Archive -LiteralPath "$SolutionData\10kRecords.zip" -DestinationPath $SolutionData -Force
+
+####Run Configure SQL to Create Databases and Populate with needed Data
+$ConfigureSql = "C:\Solutions\$SolutionName\Resources\ActionScripts\ConfigureSQL.ps1  $ServerName $SolutionName $InstallPy $InstallR $Prompt"
+Invoke-Expression $ConfigureSQL 
+
+Write-Host -ForegroundColor 'Cyan' " Done with configuration changes to SQL Server"
+
+
+
+
+Write-Host -ForeGroundColor cyan " Installing latest Power BI..."
+# Download PowerBI Desktop installer
+Start-BitsTransfer -Source "https://go.microsoft.com/fwlink/?LinkId=521662&clcid=0x409" -Destination powerbi-desktop.msi
+
+# Silently install PowerBI Desktop
+msiexec.exe /i powerbi-desktop.msi /qn /norestart  ACCEPT_EULA=1
+
+if (!$?) {
+    Write-Host -ForeGroundColor Red " Error installing Power BI Desktop. Please install latest Power BI manually."
+}
+
+
+##Create Shortcuts and Autostart Help File 
+Copy-Item "$ScriptPath\$Shortcut" C:\Users\Public\Desktop\
+Copy-Item "$ScriptPath\$Shortcut" "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\"
+Write-Host -ForeGroundColor cyan " Help Files Copied to Desktop"
+
+
+$WsShell = New-Object -ComObject WScript.Shell
+$shortcut = $WsShell.CreateShortcut($desktop + $checkoutDir + ".lnk")
+$shortcut.TargetPath = $solutionPath
+$shortcut.Save()
+
+
+
+## copy Jupyter Notebook files
+# Move-Item $SolutionPath\R\$JupyterNotebook  c:\tmp\
+# sed -i "s/XXYOURSQLPW/$password/g" c:\tmp\$JupyterNotebook
+# sed -i "s/XXYOURSQLUSER/$username/g" c:\tmp\$JupyterNotebook
+# Move-Item  c:\tmp\$JupyterNotebook $SolutionPath\R\
+
+
+
+
+#cp $SolutionData*.csv  c:\dsvm\notebooks
+ # substitute real username and password in notebook file
+#XXXXXXXXXXChange to NEw NotebookNameXXXXXXXXXXXXXXXXXX# 
+
+if ($InstallPy -eq "Yes")
+{
+    # Move-Item $SolutionPath\Python\$JupyterNotebook  c:\tmp\
+    # sed -i "s/XXYOURSQLPW/$password/g" c:\tmp\$JupyterNotebook
+    # sed -i "s/XXYOURSQLUSER/$username/g" c:\tmp\$JupyterNotebook
+    # Move-Item  c:\tmp\$JupyterNotebook $SolutionPath\Python\
+}
+
+# install modules for sample website
+if($SampleWeb  -eq "Yes")
+{
+cd $SolutionPath\Website\
+npm install
+Move-Item $SolutionPath\Website\server.js  c:\tmp\
+sed -i "s/XXYOURSQLPW/$password/g" c:\tmp\server.js
+sed -i "s/XXYOURSQLUSER/$username/g" c:\tmp\server.js
+Move-Item  c:\tmp\server.js $SolutionPath\Website
+}
+
+$endTime = Get-Date
+
+Write-Host -foregroundcolor 'green'(" $SolutionFullName Workflow Finished Successfully!")
+$Duration = New-TimeSpan -Start $StartTime -End $EndTime 
+Write-Host -ForegroundColor 'green'(" Total Deployment Time = $Duration") 
+
+Stop-Transcript
+
+
+##Launch HelpURL 
+Start-Process "https://microsoft.github.io/$SolutionFullName/Typical.html"
+
+
+
+
+## Close Powershell 
+Exit-PSHostProcess
+EXIT 
